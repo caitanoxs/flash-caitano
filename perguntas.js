0daf300f75d2cbfe0903ba06604c7d7e602f6465
caitanoxs <davi.caitano.alves@escola.pr.gov.br>
2025-06-16 18:03:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Creatina',
+    'Oque é a Creatina?',
+    'Ela desempenha um papel crucial no fornecimento de energia para os músculos'
+)
+
+criaCartao(
+    'Termogênico',
+    'Oque é um Termogênico?',
+    'Sao substâncias ou suplementos que aumentam a produção de calor no corpo,acelerando o metabolismo e promovendo a queima de gordura, auxiliando no emagrecimento e na definição muscular'
+)
+
+criaCartao(
+    'Whey protein',
+    'Oque é o Whey protein?',
+    'O whey protein serve principalmente para auxiliar no ganho de massa muscular, recuperação pós-treino e complementar a ingestão diária de proteínas, especialmente para atletas e praticantes de atividades físicas'
+)
+
+criaCartao(
+    'Esteroide anabolizante',
+    'Oque é um esteroide anabolizante?',
+    'Os esteroides , são hormônios sintéticos que estimulam o desenvolvimento de alguns tecidos do corpo a partir do crescimento da célula e sua posterior divisão. Os anabolizantes são usados no tratamento de algumas doenças, mas são usados também, em grande quantidade, por pessoas que desejam aumentar o volume dos músculos e a força física.'
+)
